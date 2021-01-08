4faa84fb596776727bf8b2fb242b96981dbac93e
MelisaOzen <melisaozennn@gmail.com>
2021-01-08 22:09:07 +0300

    First Commit

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    public float bulletDamage, lifeTime;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        
+    }
+}
